96ec39a9b83bcb0d18b68e43b58ea3f4f3c9af5d
Lokos_ed <cvier@comcast.net>
2025-04-25 22:05:44 -0400

    Delete Button Added and Working

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const tableBody = document.getElementById("tableBody")
+
+function ClearTable() {
+    const rows = tableBody.querySelectorAll('tr');
+    rows.forEach(row => row.remove());
+}
+
 async function getP() {
     try {
         const response = await fetch('http://localhost:3000/games');
@@ -5,13 +12,45 @@ async function getP() {
             const data = await response.json(); 
 
             data.forEach(element => {
-                id = data["id"]
-                title = data["title"]
-                sold = data["sold"]
 
-                console.log(element.id)
-                console.log(element.title)
-                console.log(element.sold)
+                tableRow = document.createElement("tr")
+                tableBody.appendChild(tableRow)
+
+                td1 = document.createElement("td")
+                td1.textcontent = element.id
+                let gn = document.createTextNode(td1.textcontent)
+                tableRow.appendChild(td1)
+                td1.appendChild(gn)
+
+                td2 = document.createElement("td")
+                td2.textcontent = element.title
+                let cn = document.createTextNode(td2.textcontent)
+                tableRow.appendChild(td2)
+                td2.appendChild(cn)
+
+
+                td3 = document.createElement("td")
+                td3.textcontent = element.sold
+                let rd = document.createTextNode(td3.textcontent)
+                tableRow.appendChild(td3)
+                td3.appendChild(rd)
+
+                td4 = document.createElement("button")
+                td4.textcontent = "Delete"
+                let x = document.createTextNode(td4.textcontent)
+                tableRow.appendChild(td4)
+                td4.appendChild(x)
+
+                td4.addEventListener("click", (event) => {
+                    console.log("meow")
+                    fetch(`http://localhost:3000/games/${element.id}`, {
+                        method: 'DELETE'
+                    })
+                    ClearTable()
+
+                    getP()
+                })
+
             });
 
 
@@ -23,3 +62,18 @@ async function getP() {
     }
 }
 getP()
+const submitFormButton = document.getElementById("formSubmit")
+submitFormButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    let Title = document.getElementById("gameNameInput").value
+    let Sold = document.getElementById("copiesSoldInput").value
+    fetch('http://localhost:3000/games', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: `{"title":"${Title}","sold":${Sold}}`
+    })
+    ClearTable()
+    getP()
+})
